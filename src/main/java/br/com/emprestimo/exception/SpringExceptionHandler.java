13b139f2d5594ea263bd61a825cf3b0917d2161d
Victor Hugo Marra Araujo <victor@MacBook-Pro-de-Victor.local>
2022-10-16 17:10:34 -0300

    add exception handler

---
+++ src/main/java/br/com/emprestimo/exception/SpringExceptionHandler.java
@@ -0,0 +1,23 @@
+package br.com.emprestimo.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class SpringExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = InvalidCPFException.class)
+    ResponseEntity<Object> InvalidCPFHandler(RuntimeException rtex, WebRequest request) {
+        return handleExceptionInternal(rtex, rtex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(value = UserNotFoundException.class)
+    ResponseEntity<Object> UserNotFoundHandler(RuntimeException rtex, WebRequest request) {
+        return handleExceptionInternal(rtex, rtex.getMessage(), new HttpHeaders(), HttpStatus.UNPROCESSABLE_ENTITY, request);
+    }
+}
