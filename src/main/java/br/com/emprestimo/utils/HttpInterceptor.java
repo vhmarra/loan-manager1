a2890246bdaa72f124287f7d4a1a4d74fdeb5ad1
vitin <marravh@gmail.com>
2023-09-09 20:17:21 -0300

    new implementations

--- src/main/java/br/com/emprestimo/utils/HttpInterceptor.java
+++ src/main/java/br/com/emprestimo/utils/HttpInterceptor.java
@@ -1,11 +1,9 @@
 package br.com.emprestimo.utils;
 
 import br.com.emprestimo.dtos.LogRequestDto;
-import br.com.emprestimo.enums.Topics;
-import br.com.emprestimo.kafka.producer.KafkaSender;
+import br.com.emprestimo.kafka.producer.LogProducer;
 import br.com.emprestimo.repositories.AccessTokenRepository;
 import br.com.emprestimo.services.AccessTokenService;
-import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
@@ -29,15 +27,12 @@ public class HttpInterceptor extends WebRequestHandlerInterceptorAdapter {
 
     private final AccessTokenRepository repository;
 
-    private final KafkaSender sender;
-
     private final AccessTokenService accessTokenService;
 
-    public HttpInterceptor(WebRequestInterceptor requestInterceptor, AccessTokenRepository repository, KafkaSender sender, AccessTokenService accessTokenService) {
+    public HttpInterceptor(WebRequestInterceptor requestInterceptor, AccessTokenRepository repository, AccessTokenService accessTokenService) {
         super(requestInterceptor);
         this.requestInterceptor = requestInterceptor;
         this.repository = repository;
-        this.sender = sender;
         this.accessTokenService = accessTokenService;
     }
 
@@ -46,7 +41,7 @@ public class HttpInterceptor extends WebRequestHandlerInterceptorAdapter {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addInterceptors(InterceptorRegistry registry) {
-                registry.addInterceptor(new HttpInterceptor(requestInterceptor, repository, sender, accessTokenService))
+                registry.addInterceptor(new HttpInterceptor(requestInterceptor, repository, accessTokenService))
                         .addPathPatterns("/**")
                         .excludePathPatterns("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**" /*, "/auth/**"*/);
             }
@@ -55,7 +50,6 @@ public class HttpInterceptor extends WebRequestHandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        generateLog(request);
 
         if (request.getServletPath().contains("api/v1/user") && Objects.equals(request.getMethod(), "GET")) {
             var token = repository.findByToken(request.getHeader("auth-token")).orElse(null);
@@ -80,17 +74,6 @@ public class HttpInterceptor extends WebRequestHandlerInterceptorAdapter {
         return true;
     }
 
-    private void generateLog(HttpServletRequest request) throws ServletException, IOException {
-        var logInfo = new LogRequestDto();
-        try {
-            logInfo.setMessage(request.getParameterMap().toString());
-            logInfo.setRequest(request.getParameterMap().toString());
-            var parsed = new Gson().toJson(logInfo);
-            sender.sendMessage(parsed, Topics.LOG_TOPIC.getTopicName());
-        } catch (Exception exception) {
-            log.error("Error while send log -> message {} ", exception.toString());
-        }
-    }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
