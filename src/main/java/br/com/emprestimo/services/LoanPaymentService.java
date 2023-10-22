a254324d67b947f71e4521b4b8a6416b044a1e1b
Victor Hugo Marra Araujo <marravh@gmail.com>
2023-10-21 21:36:20 -0300

    add value interest change

--- src/main/java/br/com/emprestimo/services/LoanPaymentService.java
+++ src/main/java/br/com/emprestimo/services/LoanPaymentService.java
@@ -7,6 +7,7 @@ import br.com.emprestimo.repositories.LoanPaymentsRepository;
 import br.com.emprestimo.repositories.LoanRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.formula.functions.FinanceLib;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -42,18 +43,31 @@ public class LoanPaymentService {
     }
 
     public List<LoanPaymentsEntity> createLoanPayments(LoanEntity loan) {
-        var monthsToDue = (int) ChronoUnit.MONTHS.between(loan.getLoanDateSigned().withDayOfMonth(1), loan.getLoanDateDue().withDayOfMonth(1));
+        var monthsToDue = ChronoUnit.MONTHS.between(loan.getLoanDateSigned().withDayOfMonth(1), loan.getLoanDateDue().withDayOfMonth(1));
+        var monthsToDueCasted = Long.valueOf(monthsToDue);
         var loanPayments = new ArrayList<LoanPaymentsEntity>();
+
         var monthlyValue = (loan.getLoanValue().toBigInteger().doubleValue() / monthsToDue) +
                 ((loan.getLoanValue().toBigInteger().doubleValue() / monthsToDue) * interestService.getInterestValue());
         for (int i = 0; i < monthsToDue; i++) {
             var loanPayment = new LoanPaymentsEntity();
             loanPayment.setLoan(loan);
             loanPayment.setPaymentSupposedPayDay(LocalDate.now().plusMonths(i).withDayOfMonth(1));
-            loanPayment.setPaymentValue(monthlyValue);
+            generatePaymentValue(i, loanPayment, monthlyValue, loanPayments, monthsToDueCasted);
             loan.setIsPayed(false);
             loanPayments.add(loanPayment);
         }
         return loanPayments;
+    }
+
+    private void generatePaymentValue(int i, LoanPaymentsEntity loanPayment, double monthlyValue, ArrayList<LoanPaymentsEntity> loanPayments, Long monthsToDueCasted) {
+        double lastPayment;
+        if (i == 0) {
+            loanPayment.setPaymentValue(monthlyValue);
+        } else {
+            lastPayment = loanPayments.get(i - 1).getPaymentValue();
+            var fee = lastPayment * (interestService.getInterestValue() / monthsToDueCasted.doubleValue());
+            loanPayment.setPaymentValue(lastPayment + fee);
+        }
     }
 }
